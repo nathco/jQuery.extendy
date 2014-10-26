e4d80d346422c573d8d5c6bc32f8655d99d55211
NathanRutzky <mail@nathanrutzky.com>
2014-10-26 12:26:25 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,131 @@
+# jQuery.extendy
+Common jQuery tasks re-packaged as independent utility methods. Works in tandem with jQuery’s core library.
+
+## Installation
+Include the latest version of [jQuery](http://jquery.com/download) and `jQuery.extendy.js` in the `<head>` of your HTML doc:
+```html
+<script src="jQuery.min.js"></script>  
+<script src="jQuery.extendy.js"></script>
+```
+
+## How to Use
+Reference the intended method as you typically would using normal jQuery syntax.  
+See the extended API ( below ) for a complete list of available options.
+
+**Normal**
+
+```javascript
+var timeOut;
+
+$(window).resize(function() {
+
+    clearTimeout(timeOut);
+    
+    timeOut = setTimeout(function() {
+
+        // Execute After 500ms
+        
+    }, 500);
+    
+});
+```
+**Extendy**
+```javascript
+$(window).resizeEnd(function() {
+    
+    // Execute After 500ms
+    
+}, 500);
+```
+
+## API Extended
+**.animateLeft( offset, duration, easing )**  
+`Animate the scrollLeft position of matched elements`
+
+**.animateTop( offset, duration, easing )**  
+`Animate the scrollTop position of matched elements` 
+
+**.fadeToEach( delay, duration, opacity )**  
+`Fade the opacity of each element after specified delay`
+
+**.fadeInEach( delay, duration )**  
+`Show each element by fading in after specified delay`
+
+**.fadeOutEach( delay, duration )**  
+`Hide each element by fading out after specified delay`
+
+**.toggleAttr( attr, name )**  
+`Toggle the attribute of matched elements`
+
+**.toggleId( name )**  
+`Toggle the id of matched elements`
+
+**.toggleHeight( start, end )**  
+`Toggle the height of matched elements`  
+
+**.toggleWidth( start, end )**  
+`Toggle the width of matched elements`
+
+**.fadeToggleAttr( attr, name, duration )**   
+`Fade toggle matched elements and attribute simultaneously`
+
+**.fadeToggleClass( name, duration )**  
+`Fade toggle matched elements and class simultaneously`
+
+**.fadeToggleId( name, duration )**   
+`Fade toggle matched elements and id simultaneously`
+
+**.slideToggleAttr( attr, name, duration, easing )**   
+`Slide toggle matched elements and attribute simultaneously`
+
+**.slideToggleClass( name, duration, easing )**   
+`Slide toggle matched elements and class simultaneously` 
+
+**.slideToggleId( name, duration, easing )**   
+`Slide toggle matched elements and id simultaneously` 
+
+**.slideToggleHeight( start, end, duration, easing )**    
+`Slide toggle the height of matched elements`  
+
+**.slideToggleWidth( start, end, duration, easing )**   
+`Slide toggle the width of matched elements` 
+
+**.showEach( delay )**  
+`Show each element after specified delay `
+
+**.hideEach( delay )**  
+`Hide each element after specified delay`
+
+**.nextLoop( element )**  
+`Get the next or first sibling of matched elements`
+
+**.prevLoop( element )**  
+`Get the prev or last sibling of matched elements`
+
+**.scrollBottom( )**  
+`Get the scroll position relative to the document bottom`  
+
+**.scrollRight( )**  
+`Get the scroll position relative to the document right`  
+
+**.removeAll( name, attr )**  
+`Remove class and attributes simultaneously`  
+
+**.resizeEnd( fn, delay )**  
+`Execute function after the resize event completes`
+
+**.duplicate( count )**  
+`Clone multiple copies of matched elements`
+
+**.outerHTML( )**  
+`Get the HTML fragment including matched elements`
+
+**.reverse( )**  
+`Iterate all matched elements in reverse order`
+  
+
+## Contribute
+Want to contribute? Submit a `Pull Request` with the proposed method extension—please keep it simple and use a relevant ( to existing code ) naming convention.
+
+## Feedback
+If you discover any issues or have questions regarding usage, please send a message to [mail@nath.co](mailto:mail@nath.co) or find me on twitter [@NathanRutzky](http://twitter.com/NathanRutzky).
---
+++ jQuery.extendy.js
@@ -0,0 +1,264 @@
+// jQuery tasks re-packaged as independent utility methods
+// Source: github.com/NathanRutzky/jQuery.extendy
+// Version: 1.0
+
+(function($) {
+
+    jQuery.fn.extend({
+    
+        animateLeft: function(offset, duration, easing) {
+        
+            return this.each(function() {
+            
+                $('html, body').stop().animate({
+                    
+                    scrollLeft: $(this).offset().left + offset
+                    
+                }, duration, easing);
+                                     
+            });         
+        },
+        
+        animateTop: function(offset, duration, easing) {
+        
+            return this.each(function() {
+            
+                $('html, body').stop().animate({
+                    
+                    scrollTop: $(this).offset().top + offset
+                    
+                }, duration, easing);
+                                     
+            });
+        },
+        
+        fadeToEach: function(delay, duration, opacity) {
+        
+            return this.each(function(i) {
+                
+                $(this).delay(i * delay).fadeTo(duration, opacity);
+                                 
+            });         
+        },
+        
+        fadeInEach: function(delay, duration) {
+        
+            return this.each(function(i) {
+                
+                $(this).hide().delay(i * delay).fadeIn(duration);
+                                 
+            });         
+        },
+        
+        fadeOutEach: function(delay, duration) {
+        
+            return this.reverse().each(function(i) {
+            
+                $(this).delay(i * delay).fadeOut(duration);
+                                 
+            });         
+        },
+                
+        toggleAttr: function(attr, name) {
+        
+            return this.attr(attr, function(i, val) {
+        
+                return val == name ? null : name;
+        
+            });     
+        },
+        
+        toggleId: function(name) {
+        
+            return this.attr('id', function(i, val) {
+        
+                return val == name ? null : name;
+        
+            });     
+        },
+        
+        toggleHeight: function(start, end) {
+
+            return this.each(function() {
+
+                $(this).height($(this).height() == start ? end : start);
+
+            });
+        },
+        
+        toggleWidth: function(start, end) {
+
+            return this.each(function() {
+
+                $(this).width($(this).width() == start ? end : start);
+
+            });
+        },
+        
+        fadeToggleAttr: function(attr, name, duration) {
+        
+            return this.each(function() {
+            
+                $(this).stop().fadeToggle(duration).toggleAttr(attr, name); 
+                                 
+            });         
+        },
+        
+        fadeToggleClass: function(name, duration) {
+        
+            return this.each(function() {
+            
+                $(this).stop().fadeToggle(duration).toggleClass(name);
+                                 
+            });         
+        },
+        
+        fadeToggleId: function(name, duration) {
+        
+            return this.each(function() {
+            
+                $(this).stop().fadeToggle(duration).toggleId(name); 
+                                 
+            });         
+        },
+        
+        slideToggleAttr: function(attr, name, duration, easing) {
+        
+            return this.each(function() {
+            
+                $(this).stop().slideToggle(duration, easing).toggleAttr(attr, name); 
+                                 
+            });         
+        },
+        
+        slideToggleClass: function(name, duration, easing) {
+        
+            return this.each(function() {
+            
+                $(this).stop().slideToggle(duration, easing).toggleClass(name); 
+                                 
+            });         
+        },
+        
+        slideToggleId: function(name, duration, easing) {
+        
+            return this.each(function() {
+            
+                $(this).stop().slideToggle(duration, easing).toggleId(name); 
+                                 
+            });         
+        },
+        
+        slideToggleHeight: function(start, end, duration, easing) {
+        
+            return this.each(function() {
+        
+                $(this).stop().animate({
+        
+                    height: ($(this).height() == start) ? end : start
+        
+                }, duration, easing);
+        
+            });
+        },
+        
+        slideToggleWidth: function(start, end, duration, easing) {
+        
+            return this.each(function() {
+        
+                $(this).stop().animate({
+        
+                    width: ($(this).width() == start) ? end : start
+        
+                }, duration, easing);
+        
+            });
+        },
+        
+        showEach: function(delay) {
+        
+            return this.each(function(i) {
+                
+                $(this).hide().delay(i * delay).show(0);
+                                 
+            });         
+        },
+        
+        hideEach: function(delay) {
+        
+            return this.reverse().each(function(i) {
+            
+                $(this).delay(i * delay).hide(0);
+                                 
+            });         
+        },
+
+        nextLoop: function(element) {
+        
+            return (this.next(element).length)?
+                    this.next(element):
+                    this.prevAll(element).last();        
+        },
+        
+        prevLoop: function(element) {
+        
+            return (this.prev(element).length)?
+                    this.prev(element):
+                    this.nextAll(element).last();        
+        },
+        
+        scrollBottom: function() {
+        
+            return $(document).height() - this.scrollTop() - this.height();        
+        },
+        
+        scrollRight: function() {
+        
+            return $(document).width() - this.scrollLeft() - this.width();        
+        },
+        
+        removeAll: function(name, attr) {
+        
+            return this.each(function() {
+            
+                $(this).removeClass(name).removeAttr(attr);
+                                 
+            });         
+        },
+        
+        resizeEnd: function(fn, delay) {
+    
+            var timeOut;
+    
+            return this.resize(function() {
+    
+                clearTimeout(timeOut);
+    
+                timeOut = setTimeout(function() {
+    
+                    fn.call(this);
+    
+                }, delay);
+    
+            });
+        },
+        
+        duplicate: function(count) {
+        
+            for (var x=[], i=0; i < count; i++)
+                
+            $.merge(x, this.clone(this).get());
+            
+            return this.pushStack(x);        
+        },
+        
+        outerHTML: function() {
+        
+            return (!this.length) ? this : (this[0].outerHTML);
+        },
+            
+        reverse: [].reverse
+                
+    });    	
+
+})(jQuery);
